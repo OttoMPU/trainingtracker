619566586038af44e45a01a30943b6ebc2421725
otto puistola <otto.puistola@gmail.com>
2020-11-16 15:39:40 +0200

    web security changes

--- src/main/java/hhswd20/trainingtracker/WebSecurityConfig.java
+++ src/main/java/hhswd20/trainingtracker/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package hhswd20.trainingtracker;
 
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,7 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import hhswd20.trainingtracker.web.UserDetailServiceImpl;
 
@@ -47,4 +52,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.permitAll();			
 		}
 
+		 
+		 @Bean
+		 CorsConfigurationSource corsConfigurationSource() {
+		 UrlBasedCorsConfigurationSource source =
+		 new UrlBasedCorsConfigurationSource();
+		 CorsConfiguration config = new CorsConfiguration();
+		 config.setAllowedOrigins(Arrays.asList("*"));
+		 config.setAllowedMethods(Arrays.asList("*"));
+		 config.setAllowedHeaders(Arrays.asList("*"));
+		 config.setAllowCredentials(true);
+		 config.applyPermitDefaultValues();
+		 source.registerCorsConfiguration("/**", config);
+		 return source;
+		 }
+
+		
 }
